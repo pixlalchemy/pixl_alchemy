d6e3351a9401d74e2a71db9a70ef25923bd573cb
pixlalchemy <jordan.pixlalchemy@gmail.com>
2016-06-07 00:32:33 -0600

    Setting up of front end

--- pixl_alchemy/views.py
+++ pixl_alchemy/views.py
@@ -1,6 +1,30 @@
 from pixl_alchemy import app
+from flask import url_for, render_template
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html',
+                           title='Home',
+                           index='index')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html',
+                           title='About',
+                           about='about')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html',
+                           title='Contact',
+                           contact='contact')
+
+
+@app.route('/templates')
+def templates():
+    return render_template('templates.html',
+                           title='Templates',
+                           templates='templates')
